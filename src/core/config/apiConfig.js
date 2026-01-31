e45ad9db59bdcd4233c78face2c1f93e22e0b2e5
Jaishree <jaishreeofficial29@gmail.com>
2026-01-31 23:57:15 +0530

    added base url

--- src/core/config/apiConfig.js
+++ src/core/config/apiConfig.js
@@ -2,7 +2,7 @@
 export const API_CONFIG = {
     // Using Vite proxy to avoid CORS issues in development
     // Proxy configured in vite.config.js
-    BASE_URL: '/api',
+    BASE_URL: 'https://63ztvgg2-8000.inc1.devtunnels.ms',
     ENDPOINTS: {
         LOGIN: '/user/login',
         SIGNUP: '/user/signup',
